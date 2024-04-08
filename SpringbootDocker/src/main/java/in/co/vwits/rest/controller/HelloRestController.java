da95710d638dd1f6bf0f1aeaf115bd4a6ae3a251
intiaravind <intiaravind6@gmail.com>
2024-04-08 21:03:31 +0530

    first-commit

---
+++ SpringbootDocker/src/main/java/in/co/vwits/rest/controller/HelloRestController.java
@@ -0,0 +1,18 @@
+package in.co.vwits.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+	@GetMapping("/hello")
+	public String welcome()
+	{
+		System.out.println("In welcome");
+		return "welcome to world of docker";
+		
+	}
+	
+	
+}
